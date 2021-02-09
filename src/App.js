8ba23a3ee68c3ccfbef8cd3d9662008770e2624c
Jideofor <njusticej@gmail.com>
2021-02-09 13:51:27 +0100

    fix: correct the layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import ReactMarkdown from 'react-markdown'
 
 function App() {
   const [markdown, setMarkDown] = useState('## markdown preview')
-  return <main>
-    <section className="markdown">
-      <textarea  className='input' value={markdown}
-      onChange={(e) =>setMarkDown(e.target.value)}></textarea>
-    </section>
-    <article className="result">{markdown}</article>
-  </main>
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkDown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  )
 }
 
 export default App
