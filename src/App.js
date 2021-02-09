fdaa606bda009c5dc0640a4a00b29b46ccb0b685
Jideofor <njusticej@gmail.com>
2021-02-09 12:02:14 +0100

    feat:add page structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkDown] = useState('## markdown preview')
+  return <main>
+    <section className="markdown">
+      <textarea  className='input' value={markdown}
+      onChange={(e) =>setMarkDown(e.target.value)}></textarea>
+    </section>
+    <article className="result">{markdown}</article>
+  </main>
 }
 
 export default App
